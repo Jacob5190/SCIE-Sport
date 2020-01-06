cabf4ba6f60e53ea79096decb7fedeb2f3c592d0
BuildTools <unconfigured@null.spigotmc.org>
2020-01-06 14:03:54 +0800

    Small fixes

--- src/deleteFile.java
+++ src/deleteFile.java
@@ -3,18 +3,13 @@ import Utils.DBUtils;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class deleteFile extends HttpServlet {
 	@Override
-	protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	protected void doGet (HttpServletRequest req, HttpServletResponse resp) {
 		String id = req.getParameter("id");
 		String fId = req.getParameter("fId");
-		updateDB(id, fId);
-	}
-	private static void updateDB(String id, String fId){
 		String query = String.format("UPDATE files SET file_%s = 0, f%s_format = null WHERE id = %s;", fId, fId, id);
-		//Delete file data in database.
 		DBUtils.updateDB(query);
 	}
 }
